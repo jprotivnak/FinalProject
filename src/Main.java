790830270f9e35f25902d5afdb9d627f6212779b
jprotivnak <jprotivnak@me.com>
2016-05-03 09:30:04 -0400

    Started

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.sound.sampled.*;
+import javax.swing.border.EtchedBorder;
+
+/**
+ * @author SJHSStudent
+ *
+ */
+public class Main extends JFrame {
+	
+	
+	
+	public Main() {
+		super();
+        this.setExtendedState( this.getExtendedState()|JFrame.MAXIMIZED_BOTH );
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+        
+        this.setVisable(true);
+		
+		
+		
+		
+	}
+	
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
